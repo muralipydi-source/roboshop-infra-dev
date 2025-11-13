003f9368b9c9e15d8f298a595ab4b54b31cd1f44
murali <murali.pydi@gmail.com>
2025-11-13 15:56:28 +0530

    bastion code change in 40 databases main.tf

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,5 +1,5 @@
 locals{
-    ami_id = data.aws_ami.joindevops.id
+    # ami_id = data.aws_ami.joindevops.id
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
