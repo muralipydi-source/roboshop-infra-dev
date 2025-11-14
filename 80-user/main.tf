c64e6dca14cf32541c7274c5634a34e8b8e1f834
murali <murali.pydi@gmail.com>
2025-11-14 18:16:21 +0530

    code change in main.tf

--- 80-user/main.tf
+++ 80-user/main.tf
@@ -1,5 +1,7 @@
 module "user" {
-    source = "../../terraform-aws-roboshop"
+    source =  "git::https://github.com/muralipydi-source/terraform-aws-roboshop.git?ref=main" #"../../terraform-aws-roboshop"
     component = "user"
     rule_priority = 20
 }
+
+ #"git::https://github.com/muralipydi-source/terraform-aws-securitygroup.git?ref=main"
